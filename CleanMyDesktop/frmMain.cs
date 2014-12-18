6ec71276e51622de123ed66926f4a7c3e0f67dc5
Ken Wilcox <wilcoxk@gmail.com>
2014-12-18 16:39:48 -0700

    Just starting

---
+++ CleanMyDesktop/frmMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace CleanMyDesktop
+{
+  public partial class frmMain : Form
+  {
+    public frmMain()
+    {
+      InitializeComponent();
+    }
+
+    private void frmMain_Load(object sender, EventArgs e)
+    {
+      DataHandler dh = new DataHandler();
+      grid.DataSource = dh.GetInfoTable();
+    }
+  }
+}
